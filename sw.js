66426558b3e8cbd9946b96c3ec8fcb149ac9c934
فن المسارات للخدمات الإلكترونية <fan.almasarat.es@gmail.com>
2026-01-31 22:06:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = 'direct-chat-v3';
-const ASSETS = [
-    './',
-    'index.html',
-    'style.css',
-    'script.js',
-    'manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
-    'https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap'
-];
+const cacheName = 'zol-chat-v1';
+const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
